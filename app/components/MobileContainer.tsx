6f462f513a3b5366ca49b262dd16b403b14a17a9
Rene-Nun <christiannuntkd@hotmail.com>
2025-06-07 20:11:44 -0600

    Actualizar MobileContainer.tsx

--- app/components/MobileContainer.tsx
+++ app/components/MobileContainer.tsx
@@ -7,10 +7,8 @@ interface MobileContainerProps {
 
 export default function MobileContainer({ children, className = '' }: MobileContainerProps) {
   return (
-    // Se reemplaza 'h-full' por 'h-dvh' para asegurar que el contenedor ocupe toda la altura de la pantalla.
-    <div className={`h-dvh flex flex-col bg-gray-50 ${className}`}>
+    <div className={`h-full flex flex-col bg-gray-50 ${className}`}>
       {children}
     </div>
   )
 }
-
