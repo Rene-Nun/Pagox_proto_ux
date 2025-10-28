c01c5de41c39be5987bebe1a40d0507e4cc14fef
Rene-Nun <christiannuntkd@hotmail.com>
2025-10-28 15:55:23 -0600

    Actualizar Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -42,7 +42,7 @@ export default function Header({ title, showBack = false, onBack, showLogo = fal
             <img 
               src="/images/TuristaLogo.png" 
               alt="Turista" 
-              className="h-5 w-auto object-contain"
+              className="h-10 w-auto object-contain"
             />
           </div>
         )}
