f451a5b73778b6d0275ba5e96b10efecebb34ca5
Rene-Nun <christiannuntkd@hotmail.com>
2025-11-22 19:01:59 -0700

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,6 @@ export default function Home() {
       case 'plans':
         return <PlansScreen 
           onNavigate={handleNavigation} 
-          activeTab={activeTab}
           forSaleTickets={forSaleTickets}
         />
       case 'profile':
